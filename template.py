488fd9c2a535612ba751a33ddbae0699533a7b52
Ashish Raj <ashish1195.raj@gmail.com>
2025-05-12 14:25:14 +0530

    Removed myenv from tracking and updated .gitignore

---
+++ template.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
+
+project_name = "mlproject"
+
+lists_of_file = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.py",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    
+]
+
+for filepath in lists_of_file:
+    filepath = Path(filepath)  # Use Path instead of path
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    if not filepath.exists() or filepath.stat().st_size == 0:  # Use `filepath.stat().st_size` for file size
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
