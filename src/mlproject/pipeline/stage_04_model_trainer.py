7195079e8fd49ece2c7e5ea90c64d8266c9ad958
Ashish Raj <ashish1195.raj@gmail.com>
2025-05-21 23:45:24 +0530

    modified app.py

--- src/mlproject/pipeline/stage_04_model_trainer.py
+++ src/mlproject/pipeline/stage_04_model_trainer.py
@@ -1,28 +1,36 @@
 from mlproject.config.configuration import ConfigurationManager
-# from mlproject.components.data_ingestion import DataIngestion
 from mlproject.components.model_trainer import ModelTrainer
 from mlproject import logger
 from pathlib import Path
 
-STAGE_NAME="Model Trainer stage"
+from dotenv import load_dotenv
+import mlflow
+import os
+
+STAGE_NAME = "Model Trainer stage"
 
 class MOdelTraningPipeline:
     def __init__(self):
-        pass
-    
+        # Load environment variables from .env
+        load_dotenv()
+
+        # Configure MLflow from .env
+        mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+        mlflow.set_tracking_username(os.getenv("MLFLOW_TRACKING_USERNAME"))
+        mlflow.set_tracking_password(os.getenv("MLFLOW_TRACKING_PASSWORD"))
+
     def main(self):
         config = ConfigurationManager()
-        data_transformation_config= config.get_model_trainer_config()
-        data_transformation= ModelTrainer(config=data_transformation_config)
+        data_transformation_config = config.get_model_trainer_config()
+        data_transformation = ModelTrainer(config=data_transformation_config)
         data_transformation.train()
-        
-        
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<<<")
-        obj=MOdelTraningPipeline()
+        obj = MOdelTraningPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} has completed <<<<<\n\n x========================================x")
     except Exception as e:
-        logger.exceptiona(e)
-        raise e 
+        logger.exception(e)
+        raise e
